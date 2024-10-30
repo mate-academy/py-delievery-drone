5d4e58eeb1e82d71566017fc44e16d7cf55635ae
fanni <fmalevych@zoolatech.com>
2024-10-30 15:45:45 +0200

    minor fixes

--- app/main.py
+++ app/main.py
@@ -58,6 +58,8 @@ class DeliveryDrone(FlyingRobot):
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
+        else:
+            print("Overweight. Cargo cant be loaded.")
 
     def unhook_load(self) -> None:
         self.current_load = None
