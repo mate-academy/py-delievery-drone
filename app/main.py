b122fc603cf8facad16096d0b5bc32bad61d042b
Julia Alexeeva <julia4406@gmail.com>
2024-12-09 18:33:04 +0200

    Changed logic of FlyingRobot constructor (coords applying)

--- app/main.py
+++ app/main.py
@@ -37,10 +37,9 @@ class FlyingRobot(BaseRobot):
             weight: int,
             coords: list | None = None
     ) -> None:
+        if coords is None or len(coords) != 3:
+            self.coords = coords = [0, 0, 0]
         super().__init__(name, weight, coords)
-        self.coords = coords or [0, 0, 0]
-        if len(self.coords) == 2:
-            self.coords.append(0)
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
