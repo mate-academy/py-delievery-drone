559d3c9ed3f008c8726aa3ff41f2639f3e30a1da
Dmytro <dima.sichkar2003@gmail.com>
2024-12-11 10:50:39 +0200

    After rewiev changes

--- app/main.py
+++ app/main.py
@@ -7,27 +7,19 @@ class BaseRobot:
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
         self.weight = weight
-        self.coords = coords if coords is not None else [0, 0]
+        self.coords = coords if coords else [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
-        x, y, *_ = self.coords
-        y += step
-        self.coords[1] = y
+        self.coords[1] += step
 
     def go_back(self, step: int = 1) -> None:
-        x, y, *_ = self.coords
-        y -= step
-        self.coords[1] = y
+        self.coords[1] -= step
 
     def go_right(self, step: int = 1) -> None:
-        x, y, *_ = self.coords
-        x += step
-        self.coords[0] = x
+        self.coords[0] += step
 
     def go_left(self, step: int = 1) -> None:
-        x, y, *_ = self.coords
-        x -= step
-        self.coords[0] = x
+        self.coords[0] -= step
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
@@ -38,17 +30,13 @@ class FlyingRobot(BaseRobot):
     def __init__(self, name: str, weight: int, coords: list = None) -> None:
         super().__init__(name,
                          weight,
-                         coords if coords is not None else [0, 0, 0])
+                         coords if coords else [0, 0, 0])
 
     def go_up(self, step: int = 1) -> None:
-        x, y, z = self.coords
-        z += step
-        self.coords[2] = z
+        self.coords[2] += step
 
     def go_down(self, step: int = 1) -> None:
-        x, y, z = self.coords
-        z -= step
-        self.coords[2] = z
+        self.coords[2] -= step
 
 
 class DeliveryDrone(FlyingRobot):
@@ -61,7 +49,7 @@ class DeliveryDrone(FlyingRobot):
 
         super().__init__(name,
                          weight,
-                         coords if coords is not None else [0, 0, 0])
+                         coords if coords else [0, 0, 0])
 
         self.max_load_weight = max_load_weight
         self.current_load = None
@@ -69,7 +57,9 @@ class DeliveryDrone(FlyingRobot):
             self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load is None and cargo.weight <= self.max_load_weight:
+        if (self.current_load is None
+                and cargo.weight <= self.max_load_weight
+                and isinstance(cargo, Cargo)):
             self.current_load = cargo
 
     def unhook_load(self) -> None:
