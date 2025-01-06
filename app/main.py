bba469dda7829a2ad849928e1cd97295909306a4
Dmytro Kornitskyi <d.kornitskyi@ukr.net>
2025-01-06 15:38:28 +0200

    Solution 2

--- app/main.py
+++ app/main.py
@@ -2,11 +2,14 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
-class BaseRobot(Cargo):
-    def __init__(self, name: str, weight, coords: list = None) -> None:
-        super().__init__(weight)
+class BaseRobot():
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
         self.name = name
-        self.coords = coords if coords is not None else [0, 0]
+        self.weight = weight
+        self.coords = coords
+
+        if coords is None:
+            self.coords = [0, 0]
 
     def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
@@ -25,9 +28,12 @@ class BaseRobot(Cargo):
 
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight, coords: list = None) -> None:
-        super().__init__(name, weight,
-                         coords=coords if coords is not None else [0, 0, 0])
+    def __init__(self, name: str, weight: int, coords: list = None) -> None:
+        if coords is None:
+            coords = [0, 0, 0]
+
+        super().__init__(name, weight, coords[:2])
+        self.coords = coords
 
     def go_up(self, step: int = 1) -> None:
         self.coords[2] += step
@@ -44,9 +50,6 @@ class DeliveryDrone(FlyingRobot):
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
-        else:
-            if cargo.weight <= self.max_load_weight:
-                print("Cargo exceeds the permissible weight!")
 
     def unhook_load(self) -> None:
         self.current_load = None
