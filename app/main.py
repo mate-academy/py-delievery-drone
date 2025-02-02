03791855a0b431c215f2de4a1dad123afb2b8482
Marek Smolana <msmolana@gmail.com>
2025-02-02 07:25:54 +0100

    flake8 errors corrected

--- app/main.py
+++ app/main.py
@@ -2,8 +2,10 @@ class Cargo:
     def __init__(self, weight: int) -> None:
         self.weight = weight
 
+
 class BaseRobot:
-    def __init__(self, name: str, weight: float, coords: list = [0, 0]) -> None:
+    def __init__(self, name: str, weight: float,
+                 coords: list = [0, 0]) -> None:
         self.name = name
         self.weight = weight
         self.coords = [coords[0], coords[1]]
@@ -22,10 +24,11 @@ class BaseRobot:
 
     def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
-    
+
 
 class FlyingRobot(BaseRobot):
-    def __init__(self, name: str, weight: float, coords: list = [0, 0, 0]) -> None:
+    def __init__(self, name: str, weight: float,
+                 coords: list = [0, 0, 0]) -> None:
         super().__init__(name, weight, coords[0:2])
         self.coords.append(coords[2])
 
@@ -34,16 +37,17 @@ class FlyingRobot(BaseRobot):
 
     def go_down(self, step: int = 1) -> None:
         self.coords[2] -= step
-          
+
 
 class DeliveryDrone(FlyingRobot):
-    def __init__(self, name: str, weight: float, max_load_weight: int, current_load: Cargo, coords: list = [0, 0, 0]) -> None:
+    def __init__(self, name: str, weight: float, max_load_weight: int,
+                 current_load: Cargo, coords: list = [0, 0, 0]) -> None:
         super().__init__(name, weight, coords)
         self.max_load_weight = max_load_weight
         self.current_load = current_load
 
     def hook_load(self, cargo: Cargo) -> None:
-        if self.current_load == None and self.max_load_weight >= cargo.weight:
+        if self.current_load is None and self.max_load_weight >= cargo.weight:
             self.current_load = cargo
 
     def unhook_load(self) -> None:
